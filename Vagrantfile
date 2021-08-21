d0eff70ae2798c41ed2093ca433556ded86ffa9e
Gerald Bennett <developer@drgeb.com>
2021-08-20 19:56:35 -0500

    updated vagrant to boostrap with chef

--- Vagrantfile
+++ Vagrantfile
@@ -1,4 +1,8 @@
 Vagrant.configure("2") do |config|
   config.vm.box = "generic/ubuntu2004"
   config.vm.box_version = "3.1.16"
+
+  config.vm.provision "chef_solo" do |chef|
+    chef.add_recipe "bootstrap"
+  end
 end
--- bootstrap/recipes/default.rb
+++ bootstrap/recipes/default.rb
@@ -3,3 +3,25 @@
 # Recipe:: default
 #
 # Copyright:: 2021, The Authors, All Rights Reserved.
+directory '/home/vagrant/.ssh' do
+    owner 'vagrant'
+    group 'vagrant'
+    mode '0700'
+    action :create
+end
+
+cookbook_file '/home/vagrant/.ssh/id_rsa_bb' do
+    source 'id_rsa_bb'
+    owner 'vagrant'
+    group 'vagrant'
+    mode '0600'
+    action :create
+end
+
+cookbook_file '/home/vagrant/.ssh/config' do
+    source 'config'
+    owner 'vagrant'
+    group 'vagrant'
+    mode '0600'
+    action :create
+end
