50688f1e3cbc5ee9ae0345f0b2e9c1a84819a38a
drgeb <bitbucket@drgeb.com>
2021-09-14 04:31:21 -0500

    add user drgeb

--- bootstrap/recipes/default.rb
+++ bootstrap/recipes/default.rb
@@ -33,3 +33,57 @@ cookbook_file '/home/vagrant/.ssh/.ansible_vault_system_configs' do
     mode '0600'
     action :create
 end
+
+group 'dev' do
+    append                true
+    comment               "Developers group"
+    group_name            "dev"
+    action                :create
+end
+
+user 'drgeb' do
+    comment 'A random user'
+    uid 1234
+    gid 'dev'
+    home '/home/drgeb'
+    shell '/bin/bash'
+    password '$1$JJsvHslasdfjVEroftprNn4JHtDi'
+end
+
+# Additional User
+directory '/home/drgeb/' do
+    owner 'drgeb'
+    group 'dev'
+    action :create
+end
+
+directory '/home/drgeb/.ssh' do
+    owner 'drgeb'
+    group 'dev'
+    mode '0700'
+    action :create
+end
+
+cookbook_file '/home/drgeb/.ssh/id_rsa_bb' do
+    source 'id_rsa_bb'
+    owner 'drgeb'
+    group 'dev'
+    mode '0600'
+    action :create
+end
+
+cookbook_file '/home/drgeb/.ssh/config' do
+    source 'config'
+    owner 'drgeb'
+    group 'dev'
+    mode '0600'
+    action :create
+end
+
+cookbook_file '/home/drgeb/.ssh/.ansible_vault_system_configs' do
+    source 'ansible_vault_system_configs'
+    owner 'drgeb'
+    group 'dev'
+    mode '0600'
+    action :create
+end
